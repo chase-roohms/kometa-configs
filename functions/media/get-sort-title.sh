cce13c043557de49110f56fb71676b54698ff909
Chase Roohms <131704514+chase-roohms@users.noreply.github.com>
2025-08-10 14:57:39 -0500

    Create get-sort-title.sh
    Signed-off-by: Chase Roohms <131704514+chase-roohms@users.noreply.github.com>

---
+++ functions/media/get-sort-title.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check for exactly 1 argument
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <title>"
+    exit 1
+fi
+
+title="$1"
+
+# Convert to lowercase for comparison
+lower_title="$(echo "$title" | tr '[:upper:]' '[:lower:]')"
+
+# Save default sort title
+sort_title="$title"
+
+# Remove leading articles
+for article in "a " "an " "the "; do
+    if [[ "$lower_title" == "$article"* ]]; then
+        sort_title="${title:${#article}}"
+        break
+    fi
+done
+
+echo "$sort_title" | iconv -f UTF-8 -t ASCII//TRANSLIT
