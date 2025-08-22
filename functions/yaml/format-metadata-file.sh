2bf25ce8345a1060cc81036e79bad31c1e7c667c
Chase Roohms <chaseroohms@gmail.com>
2025-08-21 19:12:53 -0500

    Automate quote formatting

--- functions/yaml/format-metadata-file.sh
+++ functions/yaml/format-metadata-file.sh
@@ -22,3 +22,18 @@ yq -i '(.metadata[] | select(has("url_poster") and .url_poster == "") | .url_pos
 
 # Single quote the url_poster for any seasons if it is empty (personal preference)
 yq -i '(.metadata[] | select(has("seasons")) | .seasons[]? | select(has("url_poster") and .url_poster == "") | .url_poster) style="single"' "$METADATA_FILE"
+
+# Single quote any label_title that contains a colon
+yq -i '(.metadata[] | select(has("label_title") and .label_title | test(":")) | .label_title) style="single"' "$METADATA_FILE"
+
+# Single quote any sort_title that contains a colon
+yq -i '(.metadata[] | select(has("sort_title") and .sort_title | test(":")) | .sort_title) style="single"' "$METADATA_FILE"
+
+# Double quote any label_title that contains a single quote
+yq -i '(.metadata[] | select(has("label_title") and .label_title | test("'\''")) | .label_title) style="double"' "$METADATA_FILE"
+
+# Double quote any sort_title that contains a single quote
+yq -i '(.metadata[] | select(has("sort_title") and .sort_title | test("'\''")) | .sort_title) style="double"' "$METADATA_FILE"
+
+# Double quote any tpdb_search that contains a single quote
+yq -i '(.metadata[] | select(has("tpdb_search") and .tpdb_search | test("'\''")) | .tpdb_search) style="double"' "$METADATA_FILE"
