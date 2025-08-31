710cf88cad013fc697b806620d8ea2931f20032e
Chase Roohms <chaseroohms@gmail.com>
2025-08-31 15:13:37 -0500

    Always download as a jpg

--- functions/tpdb/download_tpdb_image.sh
+++ functions/tpdb/download_tpdb_image.sh
@@ -25,19 +25,4 @@ if [[ "$url" =~ ^[0-9]+$ ]]; then
 fi
 
 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
-tmp_file=$(mktemp "${collection_id}.XXXXXX.tmp")
-
-# Download with single curl and extract content type from response headers
-response=$(curl -sD - -A "$user_agent" "$url" -o "$tmp_file")
-content_type=$(echo "$response" | grep -i '^Content-Type:' | awk '{print $2}' | tr -d '\r')
-
-# Map content type to extension
-case "$content_type" in
-    image/jpeg) ext="jpg" ;;
-    image/png) ext="png" ;;
-    image/webp) ext="webp" ;;
-    *) ext="img" ;;
-esac
-
-# Rename file with correct extension
-cp "$tmp_file" "${output_directory}/${collection_id}.${ext}"
+curl -s -A "$user_agent" "$url" -o "${output_directory}/${collection_id}.jpg"
